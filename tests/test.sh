bd5c9717fc89ad895d726ad5241aa5bc4e846f1a
Boris Skert <boris.skert@gmail.com>
2020-04-15 11:31:04 +0200

    Use configured publish.interface

--- tests/test.sh
+++ tests/test.sh
@@ -5,6 +5,11 @@ vagrant up --provision
 
 ansible-galaxy install -r requirements.yml -p ./roles --force
 
+echo "Waiting for answer on port 22..."
+while ! timeout 1 nc -z 192.168.33.10 22; do
+  sleep 0.2
+done
+
 ansible-playbook -i inventory.ini test.yml
 
 ansible-playbook -i inventory.ini test.yml \
@@ -12,9 +17,8 @@ ansible-playbook -i inventory.ini test.yml \
   && (echo 'Idempotence test: pass' && exit 0) \
   || (echo 'Idempotence test: fail' && exit 1)
 
-curl -s http://192.168.33.10:10080 \
- | grep -q '</html>' \
-  && (echo 'curl test: pass' && exit 0) \
-  || (echo 'curl test: fail' && exit 1)
+(nc -z -w300 192.168.33.10 10080) && \
+(echo 'Netcat test: pass' && exit 0) \
+|| (echo 'Netcat test: fail' && exit 1)
 
 vagrant destroy -f
